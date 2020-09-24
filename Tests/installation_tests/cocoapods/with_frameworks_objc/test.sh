5dae2af397de7f7bb62fd2a9eb6f82c59b929c33
David Estes <davidestes@stripe.com>
2020-09-24 15:10:26 -0700

    Fix support for Cocoapods 110

--- Tests/installation_tests/cocoapods/with_frameworks_objc/test.sh
+++ Tests/installation_tests/cocoapods/with_frameworks_objc/test.sh
@@ -32,18 +32,6 @@ if ! command -v pod > /dev/null; then
 fi
 
 # Verify cocoapods is up to date
-cocoapods_version_local="$(pod --version | grep --only-matching --extended-regexp "[0-9\.]+")"
-cocoapods_version_remote="$(gem search ^cocoapods$ --remote --no-verbose | grep --only-matching --extended-regexp "[0-9\.]+")"
-
-if [[ "${cocoapods_version_local}" != "${cocoapods_version_remote}" ]]; then
-  if [[ "${CI}" != "true" ]]; then
-    die "Please update cocoapods: \`gem update cocoapods\`"
-  fi
-
-  info "Updating cocoapods..."
-  gem update cocoapods --no-document || die "Executing \`gem update cocoapods\` failed"
-fi
-
 # Switch to script directory
 cd "${script_dir}" || die "Executing \`cd\` failed"
 
