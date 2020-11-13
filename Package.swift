493b724128214a4e65e36baff2b115be9214f00f
David Estes <davidestes@stripe.com>
2020-11-12 17:57:16 -0800

    Try something else for SPM

--- Package.swift
+++ Package.swift
@@ -14,20 +14,15 @@ let package = Package(
     )
   ],
   targets: [
-    .target(
+    .binaryTarget(
       name: "Stripe",
-      dependencies: ["Stripe3DS2"],
-      path: "Stripe",
-        exclude: ["BuildConfigurations", "Info.plist"],
-        resources: [
-          .process("Info.plist"),
-          .process("Resources/Images"),
-          .process("Resources/au_becs_bsb.json")
-        ]
+      url: "https://github.com/stripe/stripe-ios/releases/download/yuki%2FvFoo/Stripe.xcframework.zip",
+      checksum: "936098bfc2f0da071daacdd13ce8c750e696173222f8ca66228145e8db1fe27c"
     ),
     .binaryTarget(
       name: "Stripe3DS2",
-      path: "InternalFrameworks/Stripe3DS2.xcframework"
+      url: "https://github.com/stripe/stripe-ios/releases/download/yuki%2FvFoo/Stripe3DS2.xcframework.zip",
+      checksum: "6a1cc6ed21ddad4a19b9b76678ace3cd1d2ad282dedbfa875c542574a8a6f0e6"
     )
   ]
 )
